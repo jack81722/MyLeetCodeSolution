a84327c6368269524a3b08dd4ef240a874e63de5
jack81722 <jack81722@gmail.com>
2022-12-08 11:01:18 +0800

    feat: new tree

--- scenarios/tree_node.go
+++ scenarios/tree_node.go
@@ -11,6 +11,27 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func NewTreeNode(n int) *TreeNode {
+	return &TreeNode{Val: n}
+}
+
+func NewTreeNodes(n ...interface{}) []*TreeNode {
+	arr := make([]*TreeNode, len(n))
+	for i, x := range n {
+		if x == nil {
+			arr[i] = nil
+			continue
+		}
+		val := x.(int)
+		arr[i] = NewTreeNode(val)
+	}
+	return arr
+}
+
+func NewTree(n ...interface{}) *TreeNode {
+	return insertLevel(n, 0)
+}
+
 func traversal(root *TreeNode, do func(n *TreeNode)) {
 	do(root)
 	if root.Left != nil {
@@ -32,4 +53,21 @@ func (t *TreeNode) String() string {
 	}
 	traversal(t, do)
 	return str
+}
+
+func insertLevel(arr []interface{}, i int) *TreeNode {
+	var root *TreeNode
+	if i < len(arr) {
+		if arr[i] == nil {
+			return nil
+		}
+		if x, ok := arr[i].(int); ok {
+			root = NewTreeNode(x)
+			if root != nil {
+				root.Left = insertLevel(arr, 2*i+1)
+				root.Right = insertLevel(arr, 2*i+2)
+			}
+		}
+	}
+	return root
 }
