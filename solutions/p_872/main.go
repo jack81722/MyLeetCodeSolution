94aef9c8996150209d1c8c3370b9432df01b4d76
jack81722 <jack81722@gmail.com>
2022-12-08 11:01:32 +0800

    refact: new tree

--- solutions/p_872/main.go
+++ solutions/p_872/main.go
@@ -1,153 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// case1()
-	// case2()
+	case1()
+	case2()
 	case3()
 }
 
 func case1() {
-	root1 := &TreeNode{
-		Val: 3,
-		Left: &TreeNode{
-			Val: 5,
-			Left: &TreeNode{
-				Val: 6,
-			},
-			Right: &TreeNode{
-				Val: 2,
-				Left: &TreeNode{
-					Val: 7,
-				},
-				Right: &TreeNode{
-					Val: 4,
-				},
-			},
-		},
-		Right: &TreeNode{
-			Val: 1,
-			Left: &TreeNode{
-				Val: 9,
-			},
-			Right: &TreeNode{
-				Val: 8,
-			},
-		},
-	}
-	root2 := &TreeNode{
-		Val: 3,
-		Left: &TreeNode{
-			Val: 5,
-			Left: &TreeNode{
-				Val: 6,
-			},
-			Right: &TreeNode{
-				Val: 7,
-			},
-		},
-		Right: &TreeNode{
-			Val: 1,
-			Left: &TreeNode{
-				Val: 4,
-			},
-			Right: &TreeNode{
-				Val: 2,
-				Left: &TreeNode{
-					Val: 9,
-				},
-				Right: &TreeNode{
-					Val: 8,
-				},
-			},
-		},
-	}
+	root1 := NewTree(3, 5, 1, 6, 2, 9, 8, nil, nil, 7, 4)
+	root2 := NewTree(3, 5, 1, 6, 7, 4, 2, nil, nil, nil, nil, nil, nil, 9, 8)
 	fmt.Println(leafSimilar(root1, root2))
 }
 
 func case2() {
-	root1 := &TreeNode{
-		Val: 1,
-		Left: &TreeNode{
-			Val: 2,
-		},
-		Right: &TreeNode{
-			Val: 3,
-		},
-	}
-	root2 := &TreeNode{
-		Val: 1,
-		Left: &TreeNode{
-			Val: 3,
-		},
-		Right: &TreeNode{
-			Val: 2,
-		},
-	}
+	root1 := NewTree(1, 2, 3)
+	root2 := NewTree(1, 3, 2)
 	fmt.Println(leafSimilar(root1, root2))
 }
 
 func case3() {
-	root1 := &TreeNode{
-		Val: 3,
-		Left: &TreeNode{
-			Val: 5,
-			Left: &TreeNode{
-				Val: 6,
-			},
-			Right: &TreeNode{
-				Val: 2,
-				Left: &TreeNode{
-					Val: 7,
-				},
-				Right: &TreeNode{
-					Val: 4,
-				},
-			},
-		},
-		Right: &TreeNode{
-			Val: 1,
-			Left: &TreeNode{
-				Val: 9,
-			},
-			Right: &TreeNode{
-				Val: 8,
-			},
-		},
-	}
-	root2 := &TreeNode{
-		Val: 3,
-		Left: &TreeNode{
-			Val: 5,
-			Left: &TreeNode{
-				Val: 6,
-			},
-			Right: &TreeNode{
-				Val: 7,
-			},
-		},
-		Right: &TreeNode{
-			Val: 1,
-			Left: &TreeNode{
-				Val: 4,
-			},
-			Right: &TreeNode{
-				Val: 2,
-				Left: &TreeNode{
-					Val: 9,
-				},
-				Right: &TreeNode{
-					Val: 11,
-					Left: &TreeNode{
-						Val: 8,
-					},
-					Right: &TreeNode{
-						Val: 10,
-					},
-				},
-			},
-		},
-	}
+	root1 := NewTree(3, 5, 1, 6, 2, 9, 8, nil, nil, 7, 4)
+	root2 := NewTree(3, 5, 1, 6, 7, 4, 2, nil, nil, nil, nil, nil, nil, 9, 11, nil, nil, 8, 10)
 	fmt.Println(leafSimilar(root1, root2))
 }
