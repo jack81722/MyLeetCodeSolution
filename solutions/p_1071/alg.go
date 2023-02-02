0904291a2a0803a63735ef049a6a0f9b7480e6fb
jack81722 <jack81722@gmail.com>
2023-02-02 11:30:45 +0800

    feat: great solution of problem 1071

--- solutions/p_1071/alg.go
+++ solutions/p_1071/alg.go
@@ -1,33 +1,17 @@
 package main
 
 func gcdOfStrings(str1 string, str2 string) string {
-	i := 0
-	if len(str1) > len(str2) {
+	if str1 == str2 {
+		return str1
+	}
+
+	if len(str2) > len(str1) {
 		str1, str2 = str2, str1
 	}
-	// find
-	for i = len(str1); i >= 1; i-- {
-		sub := str1[:i]
-		// skip
-		if len(str2)%len(sub)+len(str1)%len(sub) != 0 {
-			continue
-		}
-		flag := true
-		for j := 0; j < len(str2); j += len(sub) {
-			if sub != str2[j:j+len(sub)] {
-				flag = false
-				break
-			}
-		}
-		for k := 0; k < len(str1); k += len(sub) {
-			if sub != str1[k:k+len(sub)] {
-				flag = false
-				break
-			}
-		}
-		if flag {
-			return sub
-		}
+
+	if str1[:len(str2)] != str2 {
+		return ""
 	}
-	return ""
+
+	return gcdOfStrings(str1[len(str2):], str2)
 }
