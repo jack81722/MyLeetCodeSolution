0cbbadb830665a7de054aa5079a4885a4dade061
jack81722 <jack81722@gmail.com>
2023-03-27 11:57:50 +0800

    feat: remove log

--- solutions/p_64/alg.go
+++ solutions/p_64/alg.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"math"
 )
 
@@ -9,7 +8,6 @@ func minPathSum(grid [][]int) int {
 	h := len(grid)
 	w := len(grid[0])
 	temp := make([]int, w*h)
-	log.Printf("h:%v, w:%v", h, w)
 	for i := w - 1; i >= 0; i-- {
 		for j := h - 1; j >= 0; j-- {
 			index := toIndex(i, j, w, h)
