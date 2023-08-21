08a57196d2d5adf0ff780dcd54169bf560c94685
DESKTOP-P50VND1\jack8 <jack81722@gmail.com>
2023-08-21 17:12:40 +0800

    feat: problem 459

---
+++ solutions/p_459/README.md
@@ -0,0 +1,3 @@
+# Problem 459: Repeated Substring Pattern
+
+Reference: https://leetcode.com/problems/repeated-substring-pattern/description/
---
+++ solutions/p_459/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	case1()
+}
+
+func case1() {
+	s := "abab"
+	fmt.Println(repeatedSubstringPattern(s))
+}
